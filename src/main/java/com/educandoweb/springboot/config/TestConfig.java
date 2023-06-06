4d62bd1eab987e923504aac66f1d498ab2412369
Patrícia Junqueira <96187596+PATRICIAJUNQUEIRA@users.noreply.github.com>
2023-06-06 16:58:57 -0300

    Camada de serviço, registro de componentes

--- src/main/java/com/educandoweb/springboot/config/TestConfig.java
+++ src/main/java/com/educandoweb/springboot/config/TestConfig.java
@@ -20,7 +20,6 @@ public class TestConfig implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TUDO que colocar aqui vai ser executado quando a aplicação for iniciada
 		
 		User u1 = new User(null, "Patricia", "Patricia@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Luiz Fernando", "Luiz.Fernando@gmail.com", "977777777", "123456");
---
+++ src/main/java/com/educandoweb/springboot/services/UserService.java
@@ -0,0 +1,27 @@
+package com.educandoweb.springboot.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.springboot.entities.User;
+import com.educandoweb.springboot.repositories.UserRepository;
+
+@Service 
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findId(Long id) {
+		Optional<User> obj=repository.findById(id);
+		return obj.get();
+	}
+}
+
