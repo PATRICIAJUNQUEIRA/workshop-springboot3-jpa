d5c6c1499f27c0f0e0809a2a7ba11760f32620bc
Patrícia Junqueira <96187596+PATRICIAJUNQUEIRA@users.noreply.github.com>
2023-06-06 10:08:39 -0300

    Banco de Dados H2 (Banco de teste em memória) e Test Profile (Perfil do projeto para teste)

--- src/main/java/com/educandoweb/springboot/entities/User.java
+++ src/main/java/com/educandoweb/springboot/entities/User.java
@@ -2,15 +2,25 @@ package com.educandoweb.springboot.entities;
 
 import java.io.Serializable;
 
-public class User implements Serializable{
-	
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
 	private String phone;
 	private String password;
-	
+
 	public User() {
 	}
 
@@ -87,9 +97,4 @@ public class User implements Serializable{
 			return false;
 		return true;
 	}
-	
-	
-	
-	
-
 }
