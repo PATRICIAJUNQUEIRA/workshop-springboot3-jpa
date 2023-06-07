b687e5e83b7d3fa46677222d3cbc0e9e5dd16249
Patrícia Junqueira <96187596+PATRICIAJUNQUEIRA@users.noreply.github.com>
2023-06-06 21:10:04 -0300

    Entidade Pedido, Data com instant e padrão ISO 8601 (Data em forma de texto)

--- src/main/java/com/educandoweb/springboot/entities/User.java
+++ src/main/java/com/educandoweb/springboot/entities/User.java
@@ -1,11 +1,14 @@
 package com.educandoweb.springboot.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -20,10 +23,13 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
-
+	
+    @OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+	
 	public User() {
 	}
-
+	
 	public User(Long id, String name, String email, String phone, String password) {
 		super();
 		this.id = id;
@@ -71,6 +77,10 @@ public class User implements Serializable {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
